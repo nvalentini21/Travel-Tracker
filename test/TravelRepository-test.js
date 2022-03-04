20cc27be657f11911ef48215b23f1a2ab63d6bcb
Nicole Valentini <ncvalentini@gmail.com>
2022-03-04 15:00:00 -0700

    Add function and instantiation tests

--- test/TravelRepository-test.js
+++ test/TravelRepository-test.js
@@ -1,19 +1,26 @@
 import TravelRepository from '..src/TravelRepository'
 import chai from 'chai';
+import allData from './test-data'
 const expect = chai.expect;
 
-describe('User Repository', () => {
+describe('Travel Repository', () => {
 
   let userRepository, allData;
 
   beforeEach(function() {
 
-    allData = {
-      allTravelerData: [],
-      allTripData: [],
-      allDestinationData: [],
-    };
-
     userRepository = new TravelRepository(allData);
 
   });
+
+  it('should be a function', function () {
+  expect(TravelRepository).to.be.a('function');
+});
+
+it('should instantiate a Travel Repository', function () {
+  expect(userRepository).to.be.an.instanceof(UserRepository);
+});
+
+it('should keep track of a collection of all traveler data', function () {
+  expect(userRepository.userData).to.eql(allData.userData);
+});
